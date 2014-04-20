f1789c624885155a273629574d703246553cb475
Karen <chleozam@gmail.com>
2014-04-20 13:45:09 +0800

    changed contact controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,27 +1,26 @@
 class ContactsController < ApplicationController
 
-  def create
-    begin
-      Notifier.contact(contact_params).deliver
-      Notifier.contact_reply(contact_params).deliver
+    def create
+    @email = params[ :email ]
+    @subject = params[ :subject ]
+    @body = params[ :body ]
+
+    if Notifier.contact(@email, @subject, @body).deliver
+      Notifier.contact_reply(@email).deliver
       render json: {
         contact: "Both mail delivered",
         valid: true
       }
-    rescue
-      begin
-        Notifier.contact_reply_error(contact_params).deliver
-      end
+    else
+      Notifier.contact_reply_error(@email, @subject, @body).deliver
       render json: {
         contact: "There has been an error",
         valid: false
       }
     end
-  end
+    # Notifier.contact(@email, @subject, @body).deliver  
+    # Notifier.contact_reply(@email).deliver
 
-  private
-
-  def contact_params
-    params.permit( :email, :subject, :body )
   end
+  
 end
