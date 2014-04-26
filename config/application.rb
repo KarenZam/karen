f88c4e7cd4654df0418e0f2469230c7935db0ef5
Karen <chleozam@gmail.com>
2014-04-26 11:53:35 +0800

    added Mandrill email set up application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,16 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+config.action_mailer.smtp_settings = {
+      :address   => "smtp.mandrillapp.com",
+      :port      => 25, # ports 587 and 2525 are also supported with STARTTLS
+      :enable_starttls_auto => true, # detects and uses STARTTLS
+      :user_name => ENV["MANDRILL_USERNAME"],
+      :password  => ENV["MANDRILL_PASSWORD"], # SMTP password is any valid API key
+      :authentication => 'login', # Mandrill supports 'plain' or 'login'
+      :domain => 'karenfarzam.com', # your domain to identify your server when connecting
+    }
+
 module Karenfarzam
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,12 +25,5 @@ Karenfarzam::Application.configure do
   # number of complex assets.
   config.assets.debug = true
 
-  config.action_mailer.smtp_settings = {
-    address: 'smtp.gmail.com',
-    port: 587,
-    domain: 'gmail.com',
-    user_name: 'kfarzam',
-    password: ENV["GMAIL_PASSWORD"],
-    authentication: 'plain',
-    enable_starttls_auto: true}
+  
 end
